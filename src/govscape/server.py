a5f141435e532f8a06b82a8b04f011c2acf3497b
Shreya Shaji <sshaji@drake.cs.washington.edu>
2025-01-19 13:21:15 -0800

    implemented a short framework for Embedding Model and outlined pipeline

--- src/govscape/server.py
+++ src/govscape/server.py
@@ -8,3 +8,26 @@ class Server:
         self.embedding_directory = config.embedding_directory
         self.index_directory = config.index_directory
         pass
+
+    # This function accepts a query to search and returns a List object of
+    # file names that it believes serves the user's request
+    def process(self, query):
+        pass
+
+    # This class represents our embedding model, as a placeholder it returns
+    # a random vector
+    class EmbeddingModel:
+        def __init__(self):
+            pass
+
+        def embed_query(query -> string):
+            
+
+
+
+
+# basic pipeline developed:
+# 1. accept a query
+# 2. run an embedding model on the query
+# 3. return a list of files that are most similar to the query
+# 4. utilize FAISS to do this
