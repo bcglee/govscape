547bdd6b6128385065b6ed762be3ac931a7ae994
Shreya Shaji <sshaji@drake.cs.washington.edu>
2025-02-03 00:01:54 -0800

    implemented faiss set up in server

--- src/govscape/config.py
+++ src/govscape/config.py
@@ -1,6 +1,9 @@
 # These classes should contain all the configuration information necessary for
 # starting the server and serving queries, respectively.
 from pdf_to_embedding import PDFsToEmbeddings
+import numpy as np
+import faiss
+import os
 
 class IndexConfig:
     def __init__(self, pdf_directory = "test_data/pdfs", embedding_directory="test_data/embeddings", index_directory="test_data/index"):
@@ -15,5 +18,25 @@ class ServerConfig:
         self.embedding_directory = index_config.embedding_directory
         self.index_directory = index_config.index_directory
         self.model = embedding_model
+        self.model.pdfs_to_embeddings()
+
+        # define k for top-k
         self.k = k
-        self.d = 512
+
+        # Creating Faiss model
+        self.d = d
+
+        self.faiss_index = faiss.IndexFlatL2(d)
+
+        # Train model on test vectors
+        self.npy_files = []
+        for root, _, files in os.walk(self.embedding_directory):
+            for file in files:
+                if file.endswith('.npy'):  # Check for .npy extension
+                    self.npy_files.append(os.path.join(root, file))
+        
+        self.arrays = [np.load(file) for file in self.npy_files]  # Load each .npy file into an array
+        for array in self.arrays:
+            print(array.shape)
+        stacked_array = np.vstack(self.arrays) 
+        self.faiss_index.add(stacked_array)
--- src/govscape/server.py
+++ src/govscape/server.py
@@ -15,8 +15,6 @@ from pdf_to_embedding import PDFsToEmbeddings
 def main():
     index_config = IndexConfig()
     server_config = ServerConfig(index_config, PDFsToEmbeddings('test_data/pdfs', 'test_data/text', 'test_data/embeddings'))
-    server_config.model.pdfs_to_embeddings()
-    # array serving
     s = Server(server_config)
     s.serve()
 
@@ -27,36 +25,17 @@ class Server:
         self.embedding_directory = config.embedding_directory
         self.index_directory = config.index_directory
         self.model = config.model
+        self.k = self.config.k
+        self.d = self.config.d
+        self.faiss_index = self.config.faiss_index
+        self.npy_files = self.config.npy_files
         pass
 
     # Accepts a Query -> Prints out k closest arrays with distance
     def serve(self):
-        print("Welcome to End-Of-Term PDF Search Server")
-
-        # define k for top-k
-        k = self.config.k
-
-        self.model.pdfs_to_embeddings()
-
-        # Creating Faiss model
-        d = self.config.d
-        faiss_index = faiss.IndexFlatL2(d)
-        print(faiss_index.is_trained)
-
-        # Train model on test vectors
-        npy_files = []
-        for root, _, files in os.walk(self.embedding_directory):
-            for file in files:
-                if file.endswith('.npy'):  # Check for .npy extension
-                    npy_files.append(os.path.join(root, file))
-        
-        arrays = [np.load(file) for file in npy_files]  # Load each .npy file into an array
-        for array in arrays:
-            print(array.shape)
-        stacked_array = np.vstack(arrays) 
-        faiss_index.add(stacked_array)
+        print("Welcome to End-Of-Term PDF Search Server")   
     
-        print("Searching against " + str(faiss_index.ntotal) + " embeddings\n")
+        print("Searching against " + str(self.faiss_index.ntotal) + " embeddings\n")
         try:
             while True:
                 query = input("Search: ")
@@ -66,11 +45,11 @@ class Server:
                 # Create random array embedding
                 query_embedding = self.model.text_to_embeddings(query)
                 # Search for the three closest arrays
-                D, I = faiss_index.search(query_embedding, k)
+                D, I = self.faiss_index.search(query_embedding, self.k)
                 print(f"This queries' embedding {query_embedding}\n")
                 for i in range(I.shape[0]):
                     for j in range(I.shape[1]):
-                         print(f"{npy_files[I[i][j]]} is at distance {D[i][j]}")
+                         print(f"{self.npy_files[I[i][j]]} is at distance {D[i][j]}")
                 print()
         except EOFError:
             print("\nThank you for using!")
