e6d6c40edc0fd014b7a3cf5799d88aa41b8f5a44
clairegong16 <claire.gong@live.com>
2025-03-09 14:17:52 -0700

    merging embedding pipeline and its tests and test conf file to main. note: there is a small multiprocess bug in generating embeddings

--- govscape/tests/embedding_pipeline_test.py
+++ govscape/tests/embedding_pipeline_test.py
@@ -3,20 +3,17 @@
 
 import pytest 
 import sys  # to find pdf_to_embedding.py
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../govscape'))) # for finding the pdf_to_embedding file
+sys.path.append('/homes/gws/cgong16/govscape/govscape/govscape/') # insert path to the pdf_to_embedding.py file here
 from reportlab.pdfgen import canvas
 from pdf_to_embedding import * 
 from reportlab.lib.pagesizes import letter
-import shutil  # for deleting test_files/text, test_files/embeddings after pytest has finished running 
+import os
 
 model = CLIPEmbeddingModel()
 embed_pipeline = PDFsToEmbeddings('test_files/pdfs', 'test_files/text', 'test_files/embeddings', model)
 pdf_directory = 'test_files/pdfs'
 txt_directory = 'test_files/text'
 embed_directory = 'test_files/embeddings'
-
-# Note: txt and embed directory will be deleted (see conftest.py) after running the pytests.
 
 # checks that the text outputs an embedding of type np.ndarray
 def test_encode_text_CLIP():
@@ -25,12 +22,9 @@ def test_encode_text_CLIP():
     assert isinstance(embedding, np.ndarray)
 
 # checks that the pdf text matches the text outputted
-def test_convert_pdf_to_txt():
-    embed_pipeline.convert_pdf_to_txt('govscape_intro.pdf')
-
-    with open("test_files/text/govscape_intro/govscape_intro_0.txt", "r", encoding="utf-8") as file:
-        text = file.read()
-        assert text == "hello my name is govscape"
+def test_convert_pdf_to_text():
+    text = embed_pipeline.convert_pdf_to_text('test_files/pdfs/govscape_intro.pdf')
+    assert text == ["hello my name is govscape"]
 
 # makes sure the number of subdirs are equal to each other, 
 # also that there are more than one txt file per subdir
@@ -70,6 +64,7 @@ def test_convert_txt_to_embedding():
 # checks for the same file structure with txt and embed and that they have the same number of embeddings and txt files
 def test_convert_txts_to_embeddings():
     embed_pipeline.convert_txts_to_embeddings()
+
     subdirs_txt = [os.path.join(txt_directory, d) for d in os.listdir(txt_directory) if os.path.isdir(os.path.join(txt_directory, d))]
     subdirs_npy = [os.path.join(embed_directory, d) for d in os.listdir(embed_directory) if os.path.isdir(os.path.join(embed_directory, d))]
     assert(len(subdirs_txt) == len(subdirs_npy))
@@ -93,3 +88,5 @@ def test_pdfs_to_embeddings():
         npy_count = sum(1 for filename in os.listdir(subdir) if os.path.isfile(os.path.join(subdir, filename)))
         assert(npy_count >= 2)
 
+
+#TODO: add functionality that deletes the embedding and txt directories when finished
