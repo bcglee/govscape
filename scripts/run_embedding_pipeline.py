38ad980b2248822be4e5b421571e09ff8ab85321
Kyle Deeds <kylebd99@gmail.com>
2025-03-14 11:52:14 -0700

    add args for different models

--- scripts/run_embedding_pipeline.py
+++ scripts/run_embedding_pipeline.py
@@ -8,6 +8,7 @@ def main():
                         epilog='')
     parser.add_argument('-p', '--pdf-directory', default='data/test_data/TechnicalReport234PDFs', help='The directory holding the pdfs that you\'d like to embed.')      # option that takes a value
     parser.add_argument('-d', '--data-directory', default='data/test_data', help='The directory where the embeddings and other metadata should be stored.')      # option that takes a value
+    parser.add_argument('-m', '--model', default='CLIP', help='The model to use for embedding.')      # option that takes a value
     parser.add_argument('-v', '--verbose', action='store_true')  # on/off flag
     args = parser.parse_args()
     
@@ -16,11 +17,15 @@ def main():
     embeddings_directory =  args.data_directory + '/embeddings'
     index_directory =  args.data_directory + '/index'
     image_directory =  args.data_directory + '/images'
-
-    processor = gs.PDFsToEmbeddings(pdf_directory, txt_directory, embeddings_directory, image_directory, gs.CLIPEmbeddingModel())
+    model = None
+    if args.model == "CLIP":
+            model = gs.CLIPEmbeddingModel()
+    elif args.model == "UAE":
+            model = gs.TextEmbeddingModel()
+    processor = gs.PDFsToEmbeddings(pdf_directory, txt_directory, embeddings_directory, image_directory, model)
     processor.pdfs_to_embeddings()
 
-    pdftojpeg = gs.PdfToJpeg(pdf_directory, image_directory, 50)
+    pdftojpeg = gs.PdfToJpeg(pdf_directory, image_directory, 100)
     pdftojpeg.convert_directory_to_jpegs()
 
 if __name__ == '__main__':
