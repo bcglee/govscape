854934997f3c5c6af65ed6fb7bebd04d7bb2fdc8
clairegong16 <claire.gong@live.com>
2025-04-03 23:02:27 -0700

    just to use the filter, but change back to use the normal server.py later

--- govscape/govscape/__init__.py
+++ govscape/govscape/__init__.py
@@ -2,4 +2,4 @@ from .config import IndexConfig, ServerConfig
 from .indexing import IndexBuilder
 from .pdf_to_embedding import PDFsToEmbeddings, CLIPEmbeddingModel
 from .pdf_to_jpeg import PdfToJpeg
-from .server import Server
+from .server_with_filter import Server
