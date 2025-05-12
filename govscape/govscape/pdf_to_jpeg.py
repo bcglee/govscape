816d3a066c4fae07050c9552890a686a265e3b22
clairegong16 <claire.gong@live.com>
2025-05-12 01:47:34 -0700

    trying to catch what exception is thrown convert_from_path

--- govscape/govscape/pdf_to_jpeg.py
+++ govscape/govscape/pdf_to_jpeg.py
@@ -31,6 +31,10 @@ class PdfToJpeg:
             convert_from_path(pdf_filename, dpi=self.dpi, output_folder=img_directory, fmt="jpeg")
         except FileNotFoundError:
             print("FILE WAS NOT FOUND")
+        except PDFPopplerTimeoutError:
+            print("timeout for image processeding exceed error")
+        except PDFSyntaxError:
+            pring("syntax in pdf errr but i think strict=true has to happen?")
         except:
             print("CONVERSION ERROR: " + pdf_filename)
             pass
