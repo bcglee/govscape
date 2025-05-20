f35bc50c4877ea45ab39dab5bd294b24c0239d7b
clairegong16 <claire.gong@live.com>
2025-05-20 16:30:29 -0700

    hi

--- govscape/govscape/s3_ec2_embedding_pipeline.py
+++ govscape/govscape/s3_ec2_embedding_pipeline.py
@@ -96,7 +96,7 @@ def batched_file_download(BATCH_SIZE, processor):
             local_path = os.path.join('downloads', file_name)  # save here?
             os.makedirs(os.path.dirname(local_path), exist_ok=True)
             s3.download_file(bucket_name, pdf, local_path)
-            local_batch.append(local_path)
+            local_batch.append(file_name)
         
         process_pdfs(local_batch, processor)  #TODO: ?? 
 
