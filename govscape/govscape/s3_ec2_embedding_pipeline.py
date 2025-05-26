b0cc72e133e5d65860a1e5d367fe279c555b1847
clairegong16 <claire.gong@live.com>
2025-05-26 01:22:28 -0700

    support for more than 1000pdfs added

--- govscape/govscape/s3_ec2_embedding_pipeline.py
+++ govscape/govscape/s3_ec2_embedding_pipeline.py
@@ -5,12 +5,13 @@ import time
 import subprocess
 import govscape as gs
 import concurrent.futures
+import shutil
 
 s3 = boto3.client('s3')
 
 # FIELDS TO SET **************************************************************************************
 
-BATCH_SIZE = 50
+BATCH_SIZE = 100
 
 # s3://bcgl-public-bucket/2008_EOT_PDFs/PDFs/
 bucket_name = 'bcgl-public-bucket'
@@ -28,7 +29,7 @@ DATA_DIR = os.path.join(PROJECT_ROOT, 'data', 'test_data')  # THIS IS WHERE THE 
 #pdf_directory = os.path.join(DATA_DIR, 'TechnicalReport234PDFs')
 pdf_directory = 'downloads'
 txt_directory = os.path.join(DATA_DIR, 'txt')
-image_directory = os.path.join(DATA_DIR, 'images')
+image_directory = os.path.join(DATA_DIR, 'img')
 img_extracted_dir = os.path.join(DATA_DIR, 'img_extracted')
 embeddings_directory = os.path.join(DATA_DIR, 'embeddings')
 img_embeddings_dir = os.path.join(DATA_DIR, 'embeddings_img_pg')
@@ -40,10 +41,43 @@ model = gs.TextEmbeddingModel()
 processor = gs.PDFsToEmbeddings(pdf_directory, txt_directory, image_directory, img_extracted_dir, 
                                 embeddings_directory, img_embeddings_dir, e_img_embed_dir, model)
 
+
+progress_path = progress.json
+
 # ****************************************************************************************************
 
 # for analyzing: 
 pipeline_times = {'first': 0, 'second': 0, 'third': 0, 'fourth': 0, 'fifth' : 0}
+
+def get_n_pdfs(limit=100000):
+    pdf_files = []
+    continuation_token = None
+
+    while True:
+        if continuation_token:
+            result = s3.list_objects_v2(Bucket=bucket_name, Prefix=pdfs_dir, ContinuationToken=continuation_token)
+        else:
+            result = s3.list_objects_v2(Bucket=bucket_name, Prefix=pdfs_dir)
+
+        contents = result.get('Contents', [])
+        pdf_keys = [obj['Key'] for obj in contents if obj['Key'].endswith('.pdf')]
+
+        remaining = limit - len(pdf_files)
+        pdf_files.extend(pdf_keys[:remaining])
+
+        if len(pdf_files) >= limit:
+            if result.get('IsTruncated'):
+                next_token = result.get('NextContinuationToken')
+                with open(progress_path, 'w') as f:
+                    json.dump({'continuation_token': next_token}, f)
+            break
+
+        if result.get('IsTruncated'):
+            continuation_token = result.get('NextContinuationToken')
+        else:
+            break
+
+    return pdf_files
 
 def upload_directory_to_s3(ec2_dir, s3_dir):
     for root, dirs, files in os.walk(ec2_dir):
@@ -80,12 +114,14 @@ def process_pdfs(pdf_files, processor):
 
 # NON-MULTITHREADED VERSION
 def batched_file_download(BATCH_SIZE, processor):
-    result = s3.list_objects_v2(Bucket=bucket_name, Prefix=pdfs_dir)
-    # Get list of pdf file names
-    pdf_files = [obj['Key'] for obj in result.get('Contents', []) if obj['Key'].endswith('.pdf')]  # note this only returns 1000
+    # result = s3.list_objects_v2(Bucket=bucket_name, Prefix=pdfs_dir)
+    # # Get list of pdf file names
+    # pdf_files = [obj['Key'] for obj in result.get('Contents', []) if obj['Key'].endswith('.pdf')]  # note this only returns 1000
 
     # temporary: 
-    pdf_files = pdf_files[0:10]
+    # pdf_files = pdf_files[0:10]
+
+    pdf_files = get_all_pdf_keys()
 
     print("Now starting with total number of PDF files: ", len(pdf_files))
     
@@ -106,6 +142,10 @@ def batched_file_download(BATCH_SIZE, processor):
         process_pdfs(local_batch, processor)  #TODO: ?? 
 
         # TODO: DELTE THE TXT FOLDERS AND OTHERS 
+        if os.path.exists('DATA_DIR'):
+            shutil.rmtree('DATA_DIR')
+        if os.path.exists('pdf_directory'):
+            shutil.rmtree('pdf_directory')
     
     overall_end_time = time.time()
 
@@ -115,62 +155,6 @@ def batched_file_download(BATCH_SIZE, processor):
     print("TOTAL TIME pdf -> img per page time:", pipeline_times['third'])
     print("TOTAL TIME img per page -> embed time:", pipeline_times['fourth'])
     print("TOTAL TIME img extracted -> embed time :", pipeline_times['fifth'])
-
-# # MULTITHREADED VERSION??? -- idk if this is really speeding up anything
-# def download_pdf(pdf, batch_download_dir):
-#     file_name = os.path.basename(pdf)
-#     local_path = os.path.join(batch_download_dir, file_name)
-#     os.makedirs(os.path.dirname(local_path), exist_ok=True)
-#     s3.download_file(bucket_name, pdf, local_path)
-#     return local_path
-
-# def batched_file_download(BATCH_SIZE):
-#     # downloading pdfs from s3
-#     start_time_load_files = time.time()
-#     result = s3.list_objects_v2(Bucket=bucket_name, Prefix=pdfs_dir)
-#     pdf_files = [obj['Key'] for obj in result.get('Contents', []) if obj['Key'].endswith('.pdf')]  # YOU ARE ONLY GETTING THE FIRST 1000, have to do something with pages??
-
-#     pdf_files = pdf_files[:100] # temporary!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! this is just for testing 
-
-#     print("TOTAL NUMBER OF PDF FILES WE ARE ABOUT TO PROCESS IS ", len(pdf_files))
-#     print("NUMBER OF THREADS FOR DOWNLOADING FILES IS ", os.cpu_count())
-
-#     local_pdf_files = []
-    
-#     with concurrent.futures.ThreadPoolExecutor() as executor: # TODO: maybe take thsi out 
-#         futures = []
-#         for pdf in pdf_files:
-#             futures.append(executor.submit(download_pdf, pdf, batch_download_dir))
-        
-#         for future in concurrent.futures.as_completed(futures):
-#             try:
-#                 local_path = future.result()
-#                 local_pdf_files.append(os.path.basename(local_path))
-#             except Exception as e:
-#                 print(f"Download failed with error: {e}")
-    
-#     print("LOCAL PDF FILES: ", local_pdf_files)
-
-#     end_time_load_files = time.time()
-
-#     start_time_process_files = time.time()
-#     # processing pdfs here in batches 
-#     for i in range(0, len(local_pdf_files), BATCH_SIZE):
-#         batch = local_pdf_files[i:i + BATCH_SIZE]
-#         process_pdfs(batch)
-#     end_time_process_files = time.time()
-    
-#     time_load = end_time_load_files - start_time_load_files
-#     time_process = end_time_process_files - start_time_process_files
-
-#     print("TOTAL TIME TO LOAD IS ", time_load)
-#     print("TOTAL TIME TO PROCESS IS ", time_process)
-#     print("TOTAL TIME IS ", (time_process + time_load))
-
-#     print("TOTAL TIME pdf -> txt time:",  pipeline_times['first'])
-#     print("TOTAL TIME txt -> embed time:", pipeline_times['second'])
-#     print("TOTAL TIME pdf -> img per page time:", pipeline_times['third'])
-#     print("TOTAL TIME img per page -> embed time:", pipeline_times['fourth'])
 
 #poetry run python s3_ec2_embedding_pipeline.py
 def main():
