2122335c24a51004f77dce3b4c531fcfc920378b
hinxcode <hinx@uw.edu>
2025-05-30 02:26:08 -0700

    chore: Fix path

--- scripts/start_api_server.py
+++ scripts/start_api_server.py
@@ -5,6 +5,10 @@ def main():
     parser = argparse.ArgumentParser(description='Start the GovScape API server')
     parser.add_argument('-p', '--pdf-directory', default='data/test_data/TechnicalReport234PDFs', help='Directory containing PDF files')
     parser.add_argument('-d', '--data-directory', default='data/test_data', help='Directory containing data files')
+    parser.add_argument('--txt-directory', default=None, help='Custom txt directory path')
+    parser.add_argument('--embeddings-directory', default=None, help='Custom embeddings directory path')
+    parser.add_argument('--index-directory', default=None, help='Custom index directory path')
+    parser.add_argument('--image-directory', default=None, help='Custom image directory path')
     parser.add_argument('-m', '--model', default='CLIP', choices=['CLIP', 'UAE'], help='The model to use for embedding.')
     parser.add_argument('-k', '--top-k', type=int, default=20, help='Number of top results to return')
     parser.add_argument('-i', '--index_type', default='Memory', help='The type of index of use')
@@ -16,10 +20,10 @@ def main():
     args = parser.parse_args()
     
     pdf_directory = args.pdf_directory
-    txt_directory =  args.data_directory + '/txt'
-    embeddings_directory =  args.data_directory + '/embeddings'
-    index_directory =  args.data_directory + '/index'
-    image_directory =  args.data_directory + '/images'
+    txt_directory, embeddings_directory, index_directory, image_directory = [
+        getattr(args, f"{dir_name}_directory") or f"{args.data_directory}/{dir_name}"
+        for dir_name in ['txt', 'embeddings', 'index', 'images']
+    ]
     
     if args.model == "CLIP":
         model = gs.CLIPEmbeddingModel()
@@ -28,7 +32,10 @@ def main():
 
     index_config = gs.IndexConfig(pdf_directory, embeddings_directory, index_directory, image_directory, args.index_type)
     i = gs.IndexBuilder(index_config)
-    i.load_index()
+    
+    if args.index_type == 'Disk':
+        i.load_index()
+    
     server_config = gs.ServerConfig(index_config, gs.PDFsToEmbeddings(pdf_directory, txt_directory, embeddings_directory, image_directory, model), i)
     server = gs.Server(server_config)
     server.run(host=args.host, port=args.port, debug=args.debug)
