d90597f764cac6e3d95ba1d9af3185a2f402f5cf
hinxcode <hinx@uw.edu>
2025-05-30 02:43:41 -0700

    chore: Fix path

--- scripts/start_api_server.py
+++ scripts/start_api_server.py
@@ -8,7 +8,7 @@ def main():
     parser.add_argument('--txt-directory', default=None, help='Custom txt directory path')
     parser.add_argument('--embeddings-directory', default=None, help='Custom embeddings directory path')
     parser.add_argument('--index-directory', default=None, help='Custom index directory path')
-    parser.add_argument('--image-directory', default=None, help='Custom image directory path')
+    parser.add_argument('--images-directory', default=None, help='Custom images directory path')
     parser.add_argument('-m', '--model', default='CLIP', choices=['CLIP', 'UAE'], help='The model to use for embedding.')
     parser.add_argument('-k', '--top-k', type=int, default=20, help='Number of top results to return')
     parser.add_argument('-i', '--index_type', default='Memory', help='The type of index of use')
@@ -20,7 +20,7 @@ def main():
     args = parser.parse_args()
     
     pdf_directory = args.pdf_directory
-    txt_directory, embeddings_directory, index_directory, image_directory = [
+    txt_directory, embeddings_directory, index_directory, images_directory = [
         getattr(args, f"{dir_name}_directory") or f"{args.data_directory}/{dir_name}"
         for dir_name in ['txt', 'embeddings', 'index', 'images']
     ]
@@ -30,7 +30,7 @@ def main():
     elif args.model == "UAE":
         model = gs.TextEmbeddingModel()
 
-    index_config = gs.IndexConfig(pdf_directory, embeddings_directory, index_directory, image_directory, args.index_type)
+    index_config = gs.IndexConfig(pdf_directory, embeddings_directory, index_directory, images_directory, args.index_type)
     i = gs.IndexBuilder(index_config)
     
     if args.index_type == 'Disk':
@@ -38,7 +38,7 @@ def main():
     
     server_config = gs.ServerConfig(
         index_config, 
-        gs.PDFsToEmbeddings(pdf_directory, txt_directory, embeddings_directory, image_directory, model), 
+        gs.PDFsToEmbeddings(pdf_directory, txt_directory, embeddings_directory, images_directory, model), 
         i,
         k=args.top_k
     )
