4b7f614314bd0209051430888c21348d34aad6f6
Kyle Deeds <kylebd99@gmail.com>
2025-06-12 13:34:11 -0700

    move to the scripts folder

--- scripts/extract_pdfs.py
+++ scripts/extract_pdfs.py
@@ -5,27 +5,6 @@ import boto3
 import pyarrow as pa
 import pyarrow.parquet as pq
 
-s3 = boto3.client('s3')
-paginator = s3.get_paginator('list_objects_v2')
-
-def extract_pdfs_to_s3(bucket_name, prefix, output_bucket, output_prefix):
-    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
-    schema = pa.schema({
-        ('pdf', pa.string()),
-        ('warc_filename', pa.string())
-    })
-    writer = pq.ParquetWriter('pdf_index.parquet', schema)
-    for page in page_iterator:
-        if 'Contents' in page:
-            for obj in page['Contents']:
-                key = obj['Key']
-                
-                print(key)
-                response = s3.get_object(Bucket=bucket_name, Key=key)
-                body = response['Body']
-                save_pdf_to_s3(body, key, output_bucket, output_prefix, schema, writer)
-    writer.close()
-
 def save_pdf_to_s3(body, warc_file, output_bucket, output_prefix, schema, writer):
     for record in ArchiveIterator(body):
         if record.rec_type == 'response':
@@ -50,6 +29,26 @@ def save_pdf_to_s3(body, warc_file, output_bucket, output_prefix, schema, writer
                     Body=file,
                     ContentType='application/pdf'
                 )
+
+def extract_pdfs_to_s3(bucket_name, prefix, output_bucket, output_prefix):
+    s3 = boto3.client('s3')
+    paginator = s3.get_paginator('list_objects_v2')
+    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
+    schema = pa.schema({
+        ('pdf', pa.string()),
+        ('warc_filename', pa.string())
+    })
+    writer = pq.ParquetWriter('pdf_index.parquet', schema)
+    for page in page_iterator:
+        if 'Contents' in page:
+            for obj in page['Contents']:
+                key = obj['Key']
+                
+                print(key)
+                response = s3.get_object(Bucket=bucket_name, Key=key)
+                body = response['Body']
+                save_pdf_to_s3(body, key, output_bucket, output_prefix, schema, writer)
+    writer.close()
 
 
 extract_pdfs_to_s3("eotarchive", "crawl-data/EOT-2020/segments/IA-019/warc/", "bcgl-public-bucket", "2020_EOT_PDFs/PDFs/")
