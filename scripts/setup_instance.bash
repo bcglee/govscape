33743d6576b6cd9f26731ac45d7c33d6f9bb36eb
kylebd99 <kdeeds@cs.washington.edu>
2025-07-11 15:03:29 -0700

    refactor setup_instance.bash to simplify Python 3.11 installation for Amazon Linux

--- scripts/setup_instance.bash
+++ scripts/setup_instance.bash
@@ -5,14 +5,11 @@ set -e
 if ! command -v python3.11 &> /dev/null; then
     echo "Installing Python 3.11..."
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        if command -v yum &> /dev/null; then
-            # CentOS/RockyOS
-            sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-            sudo yum install -y gcc gcc-c++ make
-            sudo yum install -y epel-release
-            sudo yum install -y dnf-utils
-            sudo dnf module enable -y python3.11
-            sudo yum install -y python3.11 python3.11-venv python3.11-distutils
+        if grep -q "Amazon Linux" /etc/os-release; then
+                # Amazon Linux
+                sudo yum install -y gcc gcc-c++ make
+                sudo amazon-linux-extras enable python3.11
+                sudo yum install -y python3.11 python3.11-venv python3.11-distutils
         else
             # Ubuntu/Debian
             sudo apt-get update
