0e442a54915761c6248aa523ed82bf5379024b4b
kylebd99 <kylebd99@gmail.com>
2025-07-18 17:58:50 +0000

    clear prod before running embedding pipeline

--- scripts/embeddings_pipeline.sh
+++ scripts/embeddings_pipeline.sh
@@ -2,4 +2,6 @@
 set -e
 
 # Embeddings pipeline
-poetry run python scripts/s3_ec2_embedding_pipeline.py --num_pages_to_process 1
+rm -rf data/prod
+poetry run python scripts/s3_ec2_embedding_pipeline.py --num_pages_to_process 10
+
