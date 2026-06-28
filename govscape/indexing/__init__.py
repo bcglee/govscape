ae90b40f0be6148f154a65bc4f211dfdfed48490
anjaligo <anjaligo@cs.washington.edu>
2026-06-28 19:56:10 +0000

    Fixing the errors made when fixing the merge conflict

--- govscape/indexing/__init__.py
+++ govscape/indexing/__init__.py
@@ -1,3 +1,5 @@
+# AI modified: 2026-06-28 00:00:00 efa6046e31518ce0c8f83630892d98a3b4a437bb
+from .hybrid import HybridKeywordMetadataIndex, HybridVectorMetadataIndex
 from .keyword import (
     AbstractKeywordIndex,
     LanceDBKeywordIndex,
@@ -22,6 +24,8 @@ __all__ = [
     "AbstractVectorIndex",
     "DuckDBMetadataIndex",
     "FAISSIndex",
+    "HybridKeywordMetadataIndex",
+    "HybridVectorMetadataIndex",
     "LanceDBKeywordIndex",
     "LanceDBVectorIndex",
     "LuceneKeywordIndex",
